2bd67ef8b1a72370e778b6dcbc914d260d69fde2
FanrirRC <fanrir.elecks@gmail.com>
2023-07-06 14:42:55 +0200

    Final Commit for this branch for Movement
    Any other changes are to be done in a different branch

--- Assets/Scripts/PlaneMovement.cs
+++ Assets/Scripts/PlaneMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlaneMovement : MonoBehaviour
 {
     public static int movespeed = 4; //Affects Acceleration speed
-    public float rotationSpeed = 10f; //Affects rotation speed
+    public float rotationSpeed = 40f; //Affects rotation speed
 
     void Update()
     {
@@ -15,25 +15,25 @@ public class PlaneMovement : MonoBehaviour
         //Rotating movement
         if (Input.GetKey("w"))
         {
-            transform.Rotate(rotationSpeed * Time.deltaTime / 2, 0f, 0f);
+            transform.Rotate(rotationSpeed * Time.deltaTime * 4, 0f, 0f);
             Debug.Log("Up arrow key is held down");
         }
 
         if (Input.GetKey("s"))
         {
-            transform.Rotate(-rotationSpeed * Time.deltaTime / 2, 0f, 0f);
+            transform.Rotate(-rotationSpeed * Time.deltaTime * 4, 0f, 0f);
             Debug.Log("Down arrow key is held down");
         }
 
         if (Input.GetKey("a"))
         {
-            transform.Rotate(0f, -rotationSpeed * Time.deltaTime / 2, 0f);
+            transform.Rotate(0f, -rotationSpeed * Time.deltaTime * 8, 0f);
             Debug.Log("Left arrow key is held down");
         }
 
         if (Input.GetKey("d"))
         {
-            transform.Rotate(0f, rotationSpeed * Time.deltaTime / 2, 0f);
+            transform.Rotate(0f, rotationSpeed * Time.deltaTime * 8, 0f);
             Debug.Log("Right arrow key is held down");
         }
     }
