bb5ee8748d226129b51c174ea81280c7e9e2933b
FanrirRC <fanrir.elecks@gmail.com>
2023-07-06 13:46:29 +0200

    Plane movements

---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    public static int movespeed = 4; //Affects Acceleration speed
+    public float rotationSpeed = 10f; //Affects rotation speed
+
+    void Update()
+    {
+        //continuous acceleration
+        transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+
+        //Rotating movement
+        if (Input.GetKey("w"))
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime / 2, 0f, 0f);
+            Debug.Log("Up arrow key is held down");
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.Rotate(-rotationSpeed * Time.deltaTime / 2, 0f, 0f);
+            Debug.Log("Down arrow key is held down");
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0f, -rotationSpeed * Time.deltaTime / 2, 0f);
+            Debug.Log("Left arrow key is held down");
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0f, rotationSpeed * Time.deltaTime / 2, 0f);
+            Debug.Log("Right arrow key is held down");
+        }
+    }
+}
